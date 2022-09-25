8580738e2fbc99d8166fadc8ad618260938386fe
ielmakhfi <ilias.el-makhfi@cegedim.com>
2022-09-25 06:10:57 +0200

    Add socle Tennis App

---
+++ src/main/java/com/ielmakhfi/tennisapp/TennisAppApplication.java
@@ -0,0 +1,13 @@
+package com.ielmakhfi.tennisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ielmakhfi/tennisapp/TennisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ielmakhfi.tennisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
